97788c7a22ae1a228b62d61efdf03bc73fdbc776
marapaotim <marapaotim@gmail.com>
2024-03-11 15:43:57 +0800

    add additional comment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * Define the application's command schedule.
+     * Implement Code For getting the data within 30 days that has been remove and get all ids
+     * After Getting All ids it will be pass to where in to delete 
      */
     protected function schedule(Schedule $schedule): void
     {
