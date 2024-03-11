c7623110ef8f2ab7c811bc60515119ebd3f363f6
marapaotim <marapaotim@gmail.com>
2024-03-11 15:15:50 +0800

    Add comment for tasks

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -16,6 +16,13 @@ class TaskService implements TaskServiceInterface
         $this->taskRepository = $taskRepository;
     }
 
+    /**
+     * Create for task
+     *
+     * @param $data
+     *
+     * @return Task $task
+     */
     public function create($data)
     {
         $data = array_merge($data, [
@@ -24,12 +31,28 @@ class TaskService implements TaskServiceInterface
         return $this->taskRepository->create($data);
     }
 
+    /**
+     * Update for task
+     *
+     * @param $id   id of the Task
+     * @param $data the data that to be updated
+     *
+     * @return Task $task
+     */
     public function update($id, $data)
     {
         $task = $this->taskRepository->findById($id);
         return $this->taskRepository->update($task, $data);
     }
 
+    /**
+     * Soft Delete for task
+     *
+     * @param $id   id of the Task
+     *
+     * @return void
+     */
+    
     public function remove($id)
     {
         $task = $this->taskRepository->findById($id);
@@ -38,6 +61,19 @@ class TaskService implements TaskServiceInterface
         $task->save();
     }
 
+    
+    /**
+     * Getting the paginated data
+     *
+     * @param $limit    filter limit the task display per page
+     * @param $status   filter status for the tasks data
+     * @param $sortTitle sorting of data base on title
+     * @param $sortDate sorting of data base on date created
+     * @param $searchSaveStatus filter data based on search save status
+     * @param $search   filter data base on search by title    
+     *
+     * @return Task data
+     */
     public function getTasksPaginate($limit, $status, $sortTitle, $sortDate, $searchSaveStatus, $search)
     {
         return $this->taskRepository->fetchTaskPaginate($limit, $status, $sortTitle, $sortDate, $searchSaveStatus, $search);
